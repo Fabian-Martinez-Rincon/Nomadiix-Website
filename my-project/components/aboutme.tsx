aea217e73b4ede55bb9f10a3cd0dabb038fbd6ea
Fabo xw <fabianmartinezrincon.123@gmail.com>
2023-01-10 02:47:43 -0300

    Web

--- my-project/components/aboutme.tsx
+++ my-project/components/aboutme.tsx
@@ -20,10 +20,12 @@ function aboutme() {
             variants={fadeIn('up', 'tween', 0.2 , 1)}
             className='mt-[12px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white'
         >
-            Buenas, me llamo Nomadiix, soy un desarrollador 
-            indi y me gusta a compartir el proceso de mi trabajo en mi <a href='https://www.youtube.com/@NomaDiix/featured' className=' font-extrabold text-white' target={'_black'}> canal de youtube</a>. Tambien pueden contactarme 
-            por <a href='https://www.linkedin.com/in/fabian-martinez-rincon/' className=' font-extrabold text-white' target={'_black'}>linkedin</a> . Podes contratarme para hacer un 
-            diseño de una web, desarrollarla o mantenerla esto 
+            Buenas, me llamo Nomadiix,
+             soy un desarrollador <br/>
+            indi y me gusta a compartir el proceso de <br/> 
+            mi trabajo en mi <a href='https://www.youtube.com/@NomaDiix/featured' className=' font-extrabold text-white' target={'_black'}> canal de youtube</a>. <br/>
+            Tambien pueden contactarme por <a href='https://www.linkedin.com/in/fabian-martinez-rincon/' className=' font-extrabold text-white' target={'_black'}>linkedin</a> .<br/>
+            Podes contratarme para hacer un diseño<br/> de una web, desarrollarla o mantenerla esto <br/>
             va en función de tus necesidades.
         </motion.p>
 
