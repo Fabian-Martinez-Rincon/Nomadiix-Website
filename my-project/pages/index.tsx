1ec52c0575cedc90a2629aa6753e4344adcce47b
Fabo xw <fabianmartinezrincon.123@gmail.com>
2023-01-14 22:20:50 -0300

    Cambios

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import Navbar from '../components/navbar'
 import Subnavbar from '../components/subnavbar'
 import Aboutme from '../components/aboutme'
 
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -16,9 +14,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/nomadiix.ico" />
       </Head>
-      <Navbar/>
-      <Subnavbar/>
-      <Aboutme/>
+      <main>
+        <div>
+        <Navbar/>
+        <Subnavbar/>
+        <Aboutme/>
+      </div>
+      </main>
     </>
   )
 }
